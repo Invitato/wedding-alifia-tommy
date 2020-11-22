30273b71c195f7af0e821bd646c18f0f97aaf59e
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-11-22 16:58:33 +0700

    fix: typography in mobile

--- src/components/HelloSection/index.js
+++ src/components/HelloSection/index.js
@@ -11,25 +11,31 @@ function HelloSection({ isInvitation }) {
           <div className="row">
             <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
               <h3 className="sub-title hs">BISMILLAHIRAHMANIRRAHIIM</h3>
-              <h3 className="sub-title hs">Assallamualaikum Warahmatuallahi Wabarakatuh</h3>
+              <h3 className="sub-title hs">Assalamualaikum Warahmatuallahi Wabarakatuh</h3>
               <p className="info">
-                Dengan memohon Rahmat dan Ridho Allah Subhanahu Wa Ta'ala
+                Dengan memohon Rahmat dan <br />
+                Ridho Allah Subhanahu Wa Ta'ala
                 <br />
-                Kami bermaksud menyelenggarakan pernikahan putra-putri kami
+                Kami bermaksud menyelenggarakan <br />
+                pernikahan putra-putri kami
               </p>
             </div>
           </div>
           <div className="row">
             <div className="col-md-8 col-md-offset-2 text-center">
               <h3 className="">ALIFIA DIAN FARIZHA, S.T.</h3>
-              <p className="parent">Putri Pertama dari Bapak AKBP. Ir. Saiful Fajar & Ibu drg. Susilowati</p>
+              <p className="parent">
+                Putri Pertama dari Bapak AKBP. Ir. Saiful Fajar <br />& Ibu drg. Susilowati
+              </p>
             </div>
           </div>
           {/* GROOM */}
           <div className="row">
             <div className="col-md-8 col-md-offset-2 text-center">
               <h3 className="">TOMMY ANUGRAH PUTRA, S.T.</h3>
-              <p className="parent">Putra Ketiga dari Bapak H. Taufik Hidayat & Ibu Emma Rahmi</p>
+              <p className="parent">
+                Putra Ketiga dari Bapak H. Taufik Hidayat <br />& Ibu Emma Rahmi
+              </p>
             </div>
           </div>
         </div>
